ba31ce042e8c933f914f429bc4e40104db4e8d21
wanjian <wanjian@a8sport.com>
2018-11-21 10:48:30 +0800

    第3部分

--- Django/testWeb/myapp/urls.py
+++ Django/testWeb/myapp/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /myapp/
     path('index', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /myapp/current_datetime/
     path('current_datetime', views.current_datetime, name='current_datetime'),
+    # ex: /myapp/5/
     path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # ex: /myapp/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # ex: /myapp/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Django/testWeb/myapp/views.py
+++ Django/testWeb/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import HttpResponse
 import datetime
 
@@ -27,8 +27,8 @@ def current_datetime(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
     
 # def detail(request, question_id):
 #     try:
@@ -36,6 +36,11 @@ def detail(request, question_id):
 #     except Question.DoesNotExist:
 #         raise Http404("Question does not exist")
 #     return render(request, 'myapp/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
