244c5498959ef3acb6954d4e17eaf13ebf66295c
wanjian <wanjian@a8sport.com>
2018-11-21 12:04:22 +0800

    第四部分

--- Django/testWeb/myapp/views.py
+++ Django/testWeb/myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 import datetime
 
-from .models import Question
+from .models import Question,Choice
 
 # Create your views here.
 # def index(request):
@@ -42,9 +44,51 @@ def detail(request, question_id):
     return render(request, 'myapp/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
 
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'myapp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('myapp:results', args=(question.id,)))
+
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/results.html', {'question': question})
+
+
+##### 我们将删除旧的 index, detail, 和 results 视图，并用 Django 的通用视图代替。
+class IndexView(generic.ListView):
+    template_name = 'myapp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+        
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'myapp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'myapp/results.html'
